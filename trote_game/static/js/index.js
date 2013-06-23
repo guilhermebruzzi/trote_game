deb4536afdd028ff01b7753b7467cff95fe28e77
Guilherme Heynemann Bruzzi <guibruzzi@gmail.com>
2013-06-23 18:51:10 -0300

    Iniciando desenvolvimento do fluxo

--- trote_game/static/js/index.js
+++ trote_game/static/js/index.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $(document).on('click', ".continuar", function(){
         var proximo = parseInt($(this).data("next"));
         $(".fala").addClass("hidden");
-        $(".fala-" + proximo).removeClass("hidden");
+        $(".opcoes").addClass("hidden");
+        $("." + proximo).removeClass("hidden");
     });
 });
